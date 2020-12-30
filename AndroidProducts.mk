96f745b61ba5a234c0bdc2c4c4c8701208d44665
Hưng Phan <phandinhhungvp2001@gmail.com>
2020-12-31 02:25:47 +0700

    cherish
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lithium.mk
+    $(LOCAL_DIR)/cherish_lithium.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lithium-user \
-    aosp_lithium-userdebug \
-    aosp_lithium-eng
+    cherish_lithium-user \
+    cherish_lithium-userdebug \
+    cherish_lithium-eng
